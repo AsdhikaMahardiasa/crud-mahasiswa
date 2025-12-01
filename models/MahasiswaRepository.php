6d6b3e571e9d8baa754a538682c778416ba31de6
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2025-12-01 19:32:27 +0800

    tambah fungsi read (all & find) untuk data mahasiswa

---
+++ models/MahasiswaRepository.php
@@ -0,0 +1,31 @@
+<?php
+class MahasiswaRepository {
+
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    // CREATE
+    public function create($data) {
+        $sql = "INSERT INTO mahasiswa (nama, nim, prodi, angkatan, status)
+                VALUES (:nama, :nim, :prodi, :angkatan, :status)";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute($data);
+    }
+    
+     // READ ALL
+    public function all() {
+        $stmt = $this->pdo->query("SELECT * FROM mahasiswa ORDER BY id DESC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // READ BY ID
+    public function find($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM mahasiswa WHERE id=?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+}
