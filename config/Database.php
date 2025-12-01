a7d76b735b4ca1dd22e054deb8780d91bbd9e066
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2025-12-01 13:00:49 +0800

    menghubungkan php ke mysql

---
+++ config/Database.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db   = "crud_mahasiswa";
+    private $user = "root";
+    private $pass = "dika910700";   // <-- isi password MySQL kamu di sini
+
+    public $pdo;
+
+    public function connect() {
+        try {
+            $this->pdo = new PDO(
+                "mysql:host={$this->host};dbname={$this->db}",
+                $this->user,
+                $this->pass
+            );
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->pdo;
+
+        } catch (PDOException $e) {
+            die("Koneksi gagal: " . $e->getMessage());
+        }
+    }
+}
