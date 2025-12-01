4101bcae6468ce2f4912e828cbd7cdf265cb733b
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2025-12-01 20:33:45 +0800

    tambah form input untuk create data mahasiswa

--- public/create.php
+++ public/create.php
@@ -2,3 +2,31 @@
 
 <div class="container">
 <h2>Tambah Mahasiswa</h2>
+
+<form action="store.php" method="POST">
+
+    <label>Nama</label>
+    <input type="text" name="nama" required>
+
+    <label>NIM</label>
+    <input type="text" name="nim" required>
+
+    <label>Prodi</label>
+    <select name="prodi">
+        <option value="TI">TI</option>
+        <option value="SI">SI</option>
+        <option value="DKV">DKV</option>
+    </select>
+
+    <label>Angkatan</label>
+    <input type="number" name="angkatan" required>
+
+    <label>Status</label>
+    <select name="status">
+        <option value="aktif">Aktif</option>
+        <option value="nonaktif">Nonaktif</option>
+    </select>
+
+    <button type="submit">Simpan</button>
+</form>
+</div>
