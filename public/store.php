edb014ef406331f882e36fc783d084930fb06531
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2025-12-02 13:54:58 +0800

    ambil data form POST untuk proses create mahasiswa

--- public/store.php
+++ public/store.php
@@ -5,3 +5,11 @@ require "../models/MahasiswaRepository.php";
 $db = new Database();
 $pdo = $db->connect();
 $repo = new MahasiswaRepository($pdo);
+
+$data = [
+    "nama" => $_POST["nama"],
+    "nim" => $_POST["nim"],
+    "prodi" => $_POST["prodi"],
+    "angkatan" => $_POST["angkatan"],
+    "status" => $_POST["status"]
+];
