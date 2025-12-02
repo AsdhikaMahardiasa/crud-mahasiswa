d480e3cda1764c6420dc7a7c259190e69d82d355
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2025-12-02 12:58:33 +0800

    setup edit.php (require, koneksi, dan load data mahasiswa)

---
+++ public/edit.php
@@ -0,0 +1,10 @@
+<?php
+require "../config/Database.php";
+require "../models/MahasiswaRepository.php";
+
+$db = new Database();
+$pdo = $db->connect();
+$repo = new MahasiswaRepository($pdo);
+
+$data = $repo->find($_GET['id']);
+?>
