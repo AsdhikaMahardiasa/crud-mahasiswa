2abed4c33582350f45930a18e8ca4a03038673c7
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2025-12-02 13:29:47 +0800

    ambil data form POST untuk proses update mahasiswa

--- public/update.php
+++ public/update.php
@@ -5,3 +5,12 @@ require "../models/MahasiswaRepository.php";
 $db = new Database();
 $pdo = $db->connect();
 $repo = new MahasiswaRepository($pdo);
+
+$data = [
+    "id" => $_POST["id"],
+    "nama" => $_POST["nama"],
+    "nim" => $_POST["nim"],
+    "prodi" => $_POST["prodi"],
+    "angkatan" => $_POST["angkatan"],
+    "status" => $_POST["status"]
+];
